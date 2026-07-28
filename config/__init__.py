9f161d5e130b314f124576af6e20e550297a970b
amirhossein <amirabbasghorbani354@gmail.com>
2026-07-28 14:09:18 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,17 +1,3 @@
-from .config import (
-    TRAIN_PATH,
-    TEST_PATH,
-    DATA_AUGMENTATION,
-    DATA_NORMALIZATION,
-    BATCH_SIZE,
-    DEVICE,
-    N_TRIALS,
-    MAX_EPOCHS_PER_TRIAL,
-    EARLY_STOP_PATIENCE,
-    STUDY_NAME,
-    STUDY_STORAGE,
-    FINAL_MAX_EPOCHS,
-)
-
+from .config import 
 if __name__ == "__main__":
     pass
